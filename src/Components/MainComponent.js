5640d9566505b3de42f62ada5fbe44382abdf7f0
Syed Maqthyar <maqhti@gmail.com>
2020-02-14 18:26:44 +0530

    assignment 2

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from "./ContactComponent";
 import {COMMENTS} from "../shared/comments";
 import {PROMOTIONS} from "../shared/promotions";
 import {LEADERS} from "../shared/leaders";
+import About from "./AboutComponent";
 class Main extends Component
 {
   constructor(props)
@@ -51,6 +52,7 @@ class Main extends Component
               <Route exact path={'/menu'} component={ () => <Menu dishes={this.state.dish} />  }/>
               <Route path={'/menu/:dishId'} component={dishwithid}/>
               <Route exact path={'/contactus'} component={Contact}/>
+              <Route exact path={'/aboutus'} component={()=> <About leaders={this.state.leaders}/>} />
               <Redirect to={'/home'}/>
           </Switch>
           <Footer/>
